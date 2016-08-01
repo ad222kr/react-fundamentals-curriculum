c091bc2ab38d8f1b2959df2dcc73cc5ec94b5c09
ad222kr <ad222kr@student.lnu.se>
2016-08-01 20:32:47 +0200

    add server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-var path = require("path");
-var express = require("express");
-var app = express();
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+
+app.use(express.static(path.join(__dirname, "dist")));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
+app.listen(PORT, err => {
+  if (err) console.log(err);
+  else console.info("==> Listening on port %s.", PORT);
+});
